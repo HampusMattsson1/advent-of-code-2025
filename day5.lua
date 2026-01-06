7333fbed401ebad4f2f30b62872a39ec0849a02c
Hampus Mattsson <hjm@ownit.nu>
2026-01-06 17:11:12 +0100

    Day 5 part 1 solved

--- day5.lua
+++ day5.lua
@@ -11,35 +11,40 @@ function main()
     local ingredientStartIndex = nil
 
     for i, line in ipairs(input) do
-        local lineString = tostring(line)
+        -- local lineString = tostring(line)
         -- print(lineString)
 
-        if lineString == "" then
+        if line == "" then
             ingredientStartIndex = i + 1
             break
-        end
-
-        -- loop through ranges
-        -- local rangeStart = string.gmatch(lineString, "([^-]+)")
-        local rangeStart, rangeEnd = lineString:match("([^-]+)-([^-]+)")
-        -- print(rangeStart) -- The part before the dash
-        -- print(rangeEnd)   -- The part after the dash
-        for j = rangeStart, rangeEnd do
-            local id = tostring(math.tointeger(j))
-
-            freshIngredients[id] = true
         end
     end
 
     -- ingredient loop
     for i = ingredientStartIndex, #input do
-        local ingredientId = tostring(input[i])
+        local ingredientId = math.tointeger(input[i])
         -- print(ingredientId)
         -- print(freshIngredients[ingredientId])
 
-        if freshIngredients[ingredientId] == true then
-            part1Result = part1Result + 1
+        -- Loop through ranges to check if the number is within any of them
+        -- print (ingredientId)
+        for j = 1, ingredientStartIndex - 2 do
+            -- print(input[j])
+            local rangeStart, rangeEnd = input[j]:match("([^-]+)-([^-]+)")
+            -- print(ingredientId, rangeStart, rangeEnd)
+            -- print(input[j])
+            if ingredientId >= math.tointeger(rangeStart) and ingredientId <= math.tointeger(rangeEnd) then
+                -- print("FRESH")
+                part1Result = part1Result + 1
+                break
+            end
         end
+
+        -- break
+
+        -- if freshIngredients[ingredientId] == true then
+        --     part1Result = part1Result + 1
+        -- end
     end
 
     print("Part 1 result: " .. part1Result)
